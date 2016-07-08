ca58ef381e9c3cd2f3d8d148b594876bcf4bae78
1830549576 <1830549576@qq.com>
2016-07-08 13:51:53 +0800

    calendar
    第二次上传

--- calanderTest/ViewController.m
+++ calanderTest/ViewController.m
@@ -39,6 +39,31 @@
     
     NSLog(@"===year=%ld,%ld,%ld\n===dayCount==%ld,%ld\n===weekWhat==%ld\n===weekJIJI==%ld\n=====%@",(long)year,(long)month,(long)today,dayCount,weekCount,weekWhat,weekJI,firstMonth);
     
+    [self createUI];
+}
+
+
+- (void)createUI
+{
+    NSDate *nowDate = [NSDate date];
+    
+    
+    NSInteger year = [nowDate year];
+    NSInteger  month = [nowDate month];
+    NSInteger today = [nowDate day];
+    
+    
+    NSInteger dayCount = [nowDate numberOfDaysInCurrentMonth];
+    NSInteger weekCount = [nowDate numberOfWeeksInCurrentMonth];
+    
+    NSInteger weekWhat = [nowDate firstWeekDayInMonth];
+    
+    NSInteger weekJI = [nowDate baseWeekly];
+    
+    NSDate *firstMonth = [nowDate baseFirstDayOfCurrentMonth];
+    
+    
+    NSLog(@"===year=%ld,%ld,%ld\n===dayCount==%ld,%ld\n===weekWhat==%ld\n===weekJIJI==%ld\n=====%@",(long)year,(long)month,(long)today,dayCount,weekCount,weekWhat,weekJI,firstMonth);
 }
 
 - (void)didReceiveMemoryWarning {
